86b58531b4cd773f1e2dca37ea9293ef5717ac6a
Kips Davenport <patrickjdavenport@gmail.com>
2013-08-05 14:21:22 +0100

    added devise

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ Crazybid::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+    #devise config. change for prod
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
