12c0e85bd6c305b823a552dedcdf1899feb7852d
Jeremy Goh <jg485host@gmail.com>
2013-08-08 16:31:11 +0100

    javascript update credits when bid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
 
   def show
       @user = User.find(params[:id])
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json { render :json => @user.credit }
+      end
   end
 
   def new
