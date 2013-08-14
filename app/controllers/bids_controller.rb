92bb5c8b21868587454595d21023a48ebb0f497e
Jeremy Goh <jg485host@gmail.com>
2013-08-14 18:35:38 +0100

    fixed issue with bids without credits, and bidding after end

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -19,26 +19,30 @@ class BidsController < ApplicationController
 		@listing = Listing.find(params[:listing_id])
 
 		@bids = Bid.all
-		@bid = Bid.new
-		@bid.user_id = params[:user_id]
-		@bid.listing=@listing
+		
+		unless (@listing.start_date + @listing.duration) <= Time.now || current_user.credit < @listing.credits_per_bid		#i.e. don't execute if listing has ended
+				@bid = Bid.new
+				@bid.user_id = params[:user_id]
+				@bid.listing=@listing
 
-		if @bid.save
-			@listing.current_price += 1
-			@listing.duration += @listing.time_per_bid
-			@listing.save
-			user.credit -= @listing.credits_per_bid
-			user.save
-			
-			websocket_helper[:bids].trigger 'new',{
-				id: @listing.id, 
-				current_price: @listing.current_price, 
-				current_winner: @listing.bids.last.user.user_name,
-				seconds_to_end: @listing.seconds_to_end,
-				user_credits: user.credit
-			}
-		else
-			raise #change this to some failure response
+				if @bid.save
+					@listing.current_price += 1
+					@listing.duration += @listing.time_per_bid
+					@listing.save
+					user.credit -= @listing.credits_per_bid
+					user.save
+					
+					websocket_helper[:bids].trigger 'new',{
+						id: @listing.id, 
+						current_price: @listing.current_price, 
+						current_winner: @listing.bids.last.user.user_name,
+						seconds_to_end: @listing.seconds_to_end,
+						user_credits: user.credit
+					}
+				else
+					raise #change this to some failure response
+				end
+
 		end
 
 	end
