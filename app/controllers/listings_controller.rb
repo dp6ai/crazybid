de95c5801a1907f1567b9a4c269731fc38405209
Jeremy Goh <jg485host@gmail.com>
2013-08-15 10:18:06 +0100

    added bid history for admin show page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -98,6 +98,7 @@ class ListingsController < ApplicationController
 
     def show
       @listing = Listing.find(params[:id])
+      @bids = @listing.bids.order("created_at DESC") if is_admin?
     end
 
     def active
